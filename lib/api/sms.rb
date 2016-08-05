a13baf03b41a31339ebf378ee8d522e85c1b481c
Jason Ihaia <jason@buildit.io>
2016-08-05 10:42:29 -0600

    removed unwanted files

--- lib/api/sms.rb
+++ lib/api/sms.rb
@@ -1,11 +1,7 @@
 module Api
   module Sms
     module Util; end
-    module Json; end
   end
 end
 
 require 'api/sms/util/uuid'
-require 'api/sms/json/resource'
-
-JSONAPI::Resource.send(:include, Api::Sms::Json::Resource)
